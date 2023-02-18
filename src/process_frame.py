5e3ac4e486d1cd0e17059874a6d3f14101357320
WenXinDong2018 <wxd@stanford.edu>
2023-02-18 15:39:13 -0800

    merged

--- src/process_frame.py
+++ src/process_frame.py
@@ -1,15 +1,21 @@
 import numpy as np
 import torch.nn as nn
 import torch
+from collections import defaultdict
+from dataclasses import dataclass
 
-# from dataclasses import dataclass
+frames = []
+num_frames = 11 * 2 + 1
+k = 50
+frames_average_pooled = []
 
 
-# @dataclass
-# class Frame:
-#     frame: torch.Tensor
-#     time: int
-#     average_pooled: torch.Tensor = 0.0
+@dataclass
+class Code:
+    code: torch.Tensor
+    board_freq: int
+    obstacle_freq: int
+    idx: int
 
 class FrameBuffer:
     def __init__(self, num_frames: int, kernel_size: int, is_log_buffer=False) -> None:
@@ -32,14 +38,16 @@ class FrameBuffer:
             masked_frame: Frames with movement masked out.
         """
         frame_tensor = torch.tensor(frame, dtype=float).permute(2,0,1) # type: ignore
+        self.codebook = defaultdict(tuple)  # A list of Code
+        self.codebook_distance_eps = 10
+        self.similarity_eps = 2
         self.C, self.H, self.W = frame_tensor.shape
         self.frame_buffer.append(frame_tensor)
-        self.frames_average_pooled_buffer.append(
-            self.avg_pool2d(frame_tensor)
-        )
+        self.frames_average_pooled_buffer.append(self.avg_pool2d(frame_tensor))
 
-        if ((self.is_log_buffer and len(self.frame_buffer) < 2**self.num_frames) or
-                (not self.is_log_buffer and len(self.frame_buffer) < self.num_frames)):
+        if (self.is_log_buffer and len(self.frame_buffer) < 2**self.num_frames) or (
+            not self.is_log_buffer and len(self.frame_buffer) < self.num_frames
+        ):
             return frame
 
         # Pop oldest frame
@@ -77,39 +85,74 @@ class FrameBuffer:
         return self.to_cv_frame(self.committed_frame)
 
 
-# Frames for every 2**i frames
-frames = []
-num_frames = 3
-k = 10
-frames_average_pooled = []
+    def _update_codebook(self, mask, average_pooled):
+        # Randomly select indices of average_pooled to speed up
+        C, H, W = average_pooled.shape
+        assert H == mask.shape[0]
+        assert W == mask.shape[1]
+        for i in range(H):
+            for j in range(W):
+                code = average_pooled[:, i, j]
+                match = self._find_closest_code(code, create=True)
+                if mask[i][j] > 0:
+                    match.obstacle_freq += 1
+                else:
+                    match.board_freq += 1
 
+    def _most_likely_obstacle(self, code):
+        return code.board_freq > 5 and code.obstacle_freq / code.board_freq > 2
 
-def process_frame(frame):
-    print('Deprecated function. Use FrameBuffer class instead.')
-    global frames, frames_average_pooled, frame_counters
+    def _most_likely_board(self, code):
+        return code.obstacle_freq > 5 and code.board_freq / code.obstacle_freq > 2
 
-    frame_t = torch.tensor(frame, dtype=float).permute(2,0,1) # type: ignore
-    C, H, W = frame_t.shape
+    def _find_closest_code(self, x, create=False):
 
-    frames.append(frame_t)
+        add_code = True
+        min_dist = float("inf")
+        closest_code = None
+        target = (int(x[0]), int(x[1]), int(x[2]))
+        # print("target", target)
+        # print()
+        for r in range(
+            max(0, target[0] - self.codebook_distance_eps),
+            min(255, target[0] + self.codebook_distance_eps),
+        ):
+            for g in range(
+                max(0, target[1] - self.codebook_distance_eps),
+                min(255, target[1] + self.codebook_distance_eps),
+            ):
+                for b in range(
+                    max(0, target[2] - self.codebook_distance_eps),
+                    min(255, target[2] + self.codebook_distance_eps),
+                ):
+                    if (r, g, b) in self.codebook:
+                        add_code = False
+                        code = self.codebook[(r, g, b)]
+                        dist = torch.abs(code.code - x).sum() / 3
+                        if dist < min_dist:
+                            min_dist = dist
+                            closest_code = code
 
-    average_pool = nn.AvgPool2d((k,k), stride = (k,k))(frame_t)
-    frames_average_pooled.append(average_pool)
+        if create and add_code:
+            self.codebook[target] = Code(x, 0, 0, len(self.codebook))
+            closest_code = self.codebook[target]
+            print("adding code", len(self.codebook))
+        return closest_code
 
-    if len(frames)<num_frames:
-        return frame
+    # def update_code(code, match_idx):
+    #     match = obstacle_codebook_freq[match_idx]
+    #     freq = obstacle_codebook_freq[match_idx]
+    #     obstacle_codebook_freq[match_idx] = (freq*match + code)/ (freq+1)
+    #     obstacle_codebook_freq[match_idx]+=1
 
-    if len(frames)>num_frames:
-        frames = frames[1:]
-        frames_average_pooled = frames_average_pooled[1:]
-
-    curr_t = (num_frames-1)
-    curr_average_pooled_frame = frames_average_pooled[curr_t]
-
-    average_delta = torch.abs(curr_average_pooled_frame - torch.stack(frames_average_pooled)).sum(axis=1).mean(axis=0)/3 # type: ignore #(N, C, H, W) => (N, H, W) => (H, W)
-    mask = average_delta > 2
-    mask = nn.Upsample((H, W))(mask.unsqueeze(0).unsqueeze(0)+0.0).squeeze()
-
-    frames[curr_t][:,mask>0] = 255
-    ret = frames[curr_t]
-    return ret.permute(1,2,0).numpy().astype(np.uint8)
+    def _update_mask(self, mask, average_pooled):
+        C, H, W = average_pooled.shape
+        for i in range(H):
+            for j in range(W):
+                code = average_pooled[:, i, j]
+                match = self._find_closest_code(code, create=False)
+                if match != None:
+                    if self._most_likely_obstacle(match):
+                        mask[i, j] = 1
+                    elif self._most_likely_board(match):
+                        mask[i, j] = 0
