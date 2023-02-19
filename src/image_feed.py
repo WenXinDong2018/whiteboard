adde63317526f5fd239197918b67da77b629c2dd
stadwalkar <sahiltadwalkar@gmail.com>
2023-02-19 10:16:10 -0800

    before merge

--- src/image_feed.py
+++ src/image_feed.py
@@ -1,34 +1,111 @@
+from __future__ import annotations
+
 import cv2
+
 import process_frame as pf
+import time
+from typing import Union
 
-video_input = "../videos/terry_tao_low_res.mp4"
+class ImageFeed:
+    def __init__(self, video_input: Union[str, int],
+                 frame_buffers: list[pf.FrameBuffer] | pf.FrameBuffer,
+                 frame_buffer_names: list[str] | str | None = None,
+                 verbose: bool = False) -> None:
+        """
+        Image feed class that wraps video capturing and processing.
 
-vid = cv2.VideoCapture(video_input)
+        Args:
+            video_input (Union[str, int]): Input string to path of video or 0 for webcam feed.
+            frame_buffers (list[pf.FrameBuffer]): List of frame buffers to process frames with.
+            frame_buffer_names (list[str]): List of names for each frame buffer, used for window management.
+            verbose (bool): Whether to print verbose output.
+        """
+        # Frame buffer validation
+        if isinstance(frame_buffers, pf.FrameBuffer) and isinstance(frame_buffer_names, list) and len(frame_buffer_names) != 1:
+            raise ValueError('Frame buffer and frame buffer names must be both lists or FrameBuffer and str respectively.')
+        elif isinstance(frame_buffers, list) and isinstance(frame_buffer_names, list):
+            if frame_buffer_names is not None and len(frame_buffers) != len(frame_buffer_names):
+                raise ValueError('Number of frame buffers and frame buffer names must be equal.')
 
-frame_buffer_log = pf.FrameBuffer(6, 10, is_log_buffer=True)
-frame_buffer_lin = pf.FrameBuffer(6, 10, is_log_buffer=False)
+        if isinstance(frame_buffers, pf.FrameBuffer):
+            frame_buffers = [frame_buffers]
+        if isinstance(frame_buffer_names, str):
+            frame_buffer_names = [frame_buffer_names]
 
-while True:
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
+        if isinstance(video_input, int) and video_input != 0:
+            print('Invalid input for webcam feed. Defaulting to webcam feed.')
+            video_input = 0
+        self.video_input = video_input
+        self.windows: list[str] = []
+        self.frame_buffers = frame_buffers
+        if frame_buffer_names is None:
+            frame_buffer_names = [f'frame buffer {i}' for i in range(len(frame_buffers))]
+        self.frame_buffer_names = frame_buffer_names
+        self.windows = [f'{frame_buffer_name} masked' for frame_buffer_name in frame_buffer_names] + \
+            [f'{frame_buffer_name} committed' for frame_buffer_name in frame_buffer_names]
+        self.verbose = verbose
 
-    if not ret:
-        break
+    def _start_feed(self):
+        self.vid_capture = cv2.VideoCapture(self.video_input)
 
-    # Display the resulting frame
-    # print(frame)
-    processed_frame_log = frame_buffer_log.process_frame(frame)
-    processed_frame_lin = frame_buffer_lin.process_frame(frame)
-    # processed_frame = pf.process_frame(frame)
-    cv2.imshow('log frame', processed_frame_log)
-    cv2.imshow('lin frame', processed_frame_lin)
+    def _stop_feed(self):
+        self.vid_capture.release()
+        for win in self.windows:
+            cv2.destroyWindow(win)
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    def run_capture_loop(self):
+        self._start_feed()
+        i = 0
+        avg_scope = 100
+        start_time = time.time()
+        global_start_time = start_time
+        try:
+            while True:
+                i += 1
+                if i % avg_scope == 0 and self.verbose:
+                    print(f'Frames {i-avg_scope}-{i} average time spent: {(time.time() - start_time) / avg_scope}s per frame')
+                    start_time = time.time()
+                ret, frame = self.vid_capture.read()
+                if not ret:
+                    break
+                for frame_buffer, frame_buffer_name in zip(self.frame_buffers, self.frame_buffer_names):
+                    masked_frame = frame_buffer.process_frame(frame)
+                    committed_frame = frame_buffer.commit_frame()
+                    cv2.imshow(f'{frame_buffer_name} masked', masked_frame)
+                    cv2.imshow(f'{frame_buffer_name} committed', committed_frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        except Exception as e:
+            print(e)
+            print('Error in capture loop.')
+        self._stop_feed()
+
+        end = time.time()
+        print(f'Average time spent per frame: {(end - global_start_time) / i}s per frame')
+        print(f'Average FPS: {i / (end - global_start_time)}')
+        print(f'Frames processed: {i}')
+        print(f'Total time: {end - global_start_time}s')
+
+
+if __name__ == '__main__':
+    video_input = "../videos/terry_tao_low_res.mp4"
+
+    lin_frame_buffer = pf.FrameBuffer(50, 10, is_log_buffer=False)
+    log_frame_buffer = pf.FrameBuffer(6, 10, is_log_buffer=True)
+
+    # image_feed = ImageFeed(
+    #     0,                                      # Webcam feed
+    #     [lin_frame_buffer, log_frame_buffer],   # Frame buffers
+    #     ['linear', 'log'],                      # Frame buffer names
+    #     True                                    # Verbose
+    # )
+
+    image_feed = ImageFeed(
+        video_input,                                      # Webcam feed
+        log_frame_buffer,                       # Frame buffer
+        'log',                                  # Frame buffer name
+        True
+    )
 
 # After the loop release the cap object
 vid.release()
--- src/process_frame.py
+++ src/process_frame.py
@@ -1,15 +1,16 @@
 import numpy as np
 import torch.nn as nn
 import torch
+from collections import defaultdict
+from dataclasses import dataclass
+from scipy.ndimage.measurements import label
 
-# from dataclasses import dataclass
-
-
-# @dataclass
-# class Frame:
-#     frame: torch.Tensor
-#     time: int
-#     average_pooled: torch.Tensor = 0.0
+@dataclass
+class Code:
+    code: torch.Tensor
+    board_freq: int
+    obstacle_freq: int
+    idx: int
 
 class FrameBuffer:
     def __init__(self, num_frames: int, kernel_size: int, is_log_buffer=False) -> None:
@@ -18,18 +19,28 @@ class FrameBuffer:
         self.k = kernel_size
         self.num_frames = num_frames
         self.is_log_buffer = is_log_buffer
-        self.avg_pool2d = nn.AvgPool2d((self.k,self.k), stride = (self.k,self.k))
+        self.avg_pool2d = nn.AvgPool2d((self.k,self.k), stride = (self.k//2,self.k//2))
+        self.committed_frame = None
+        self.codebook = defaultdict(tuple)  # A list of Code
+        self.codebook_distance_eps = 10
+        self.similarity_eps = 2
 
     def process_frame(self, frame):
-        frame_tensor = torch.tensor(frame, dtype=float).permute(2,0,1)
-        self.C, self.H, self.W = frame_tensor.shape
+        """
+        Adds the frame to buffer and masks frame based on movement from previous frames.
+        Args:
+            frame (NDArray): Raw frame from video feed.
+        Returns:
+            masked_frame: Frames with movement masked out.
+        """
+        frame_tensor = torch.tensor(frame, dtype=float).permute(2,0,1) # type: ignore
+        C, H, W = frame_tensor.shape
         self.frame_buffer.append(frame_tensor)
-        self.frames_average_pooled_buffer.append(
-            self.avg_pool2d(frame_tensor)
-        )
+        self.frames_average_pooled_buffer.append(self.avg_pool2d(frame_tensor))
 
-        if ((self.is_log_buffer and len(self.frame_buffer) < 2**self.num_frames) or
-                (not self.is_log_buffer and len(self.frame_buffer) < self.num_frames)):
+        if (self.is_log_buffer and len(self.frame_buffer) < 2**self.num_frames) or (
+            not self.is_log_buffer and len(self.frame_buffer) < self.num_frames
+        ):
             return frame
 
         # Pop oldest frame
@@ -39,57 +50,135 @@ class FrameBuffer:
         current_frame_avg_pooled = self.frames_average_pooled_buffer[-1]
         if self.is_log_buffer:
             indexes = list(-1 - 2 ** i for i in range(self.num_frames))
-            average_delta = torch.abs(current_frame_avg_pooled - torch.stack(tuple(self.frames_average_pooled_buffer[i] for i in indexes))).sum(axis=1).mean(axis=0)/3
+            average_delta = torch.abs(current_frame_avg_pooled - torch.stack(tuple(self.frames_average_pooled_buffer[i] for i in indexes))).sum(axis=1).mean(axis=0)/3 # type: ignore
         else:
-            average_delta = torch.abs(current_frame_avg_pooled - torch.stack(self.frames_average_pooled_buffer)).sum(axis=1).mean(axis=0)/3 #(N, C, H, W) => (N, H, W) => (H, W)
+            average_delta = torch.abs(current_frame_avg_pooled - torch.stack(self.frames_average_pooled_buffer)).sum(axis=1).mean(axis=0)/3 # type: ignore #(N, C, H, W) => (N, H, W) => (H, W)
 
-        mask = average_delta > 2
-        mask = nn.Upsample((self.H, self.W))(mask.unsqueeze(0).unsqueeze(0)+0.0).squeeze()
+        mask = average_delta > 1
 
-        self.frame_buffer[-1][:,mask>0] = 255
-        ret = self.frame_buffer[-1]
-        return ret.permute(1,2,0).numpy().astype(np.uint8)
+        # self._update_codebook(mask, current_frame_avg_pooled)
+        self._fill_mask_gaps(mask)
+
+        mask = nn.Upsample((H, W))(mask.unsqueeze(0).unsqueeze(0)+0.0).squeeze()
+
+        self.frame_buffer[-1][:,mask>0] = -1
+        return self.to_cv_frame(self.frame_buffer[-1])
+
+    def _fill_mask_gaps(self, mask):
+
+        structure = np.ones((3, 3), dtype=np.int)
+
+        # Get rid of blinking noises
+        labeled, ncomponents = label(mask, structure)
+        labeled = np.array(labeled)
+
+        for i in range(1, ncomponents):
+            component_size = np.sum(labeled == i)
+            if component_size < 15:
+                # print(f"component with size {component_size} is background")
+                #component is background
+                mask[labeled==i] = 0
+
+        # Fill obstacles
+        labeled, ncomponents = label(torch.logical_not(mask), structure)
+        max_component_size = 0
+        for i in range(1, ncomponents):
+            component_size = np.sum(labeled == i)
+            max_component_size = max(max_component_size, component_size)
+
+        for i in range(1,ncomponents):
+            component_size = np.sum(labeled == i)
+            if component_size < max_component_size:
+                #component is obstacle
+                # print(f"component with size {component_size} is obstacle")
+                mask[labeled==i] = 1
 
 
-# Frames for every 2**i frames
-frames = []
+    def to_cv_frame(self, frame):
+        """
+        Converts a tensorflow frame (channel, height, width) to a cv2 frame (height, width, channel)
+        """
+        return frame.permute(1,2,0).numpy().astype(np.uint8)
 
-num_frames = 3
+    def commit_frame(self):
+        """
+        Commits current frame to be used as reference frame for future frames.
+        """
+        if self.committed_frame is None:
+            self.committed_frame = self.frame_buffer[-1]
+            return self.to_cv_frame(self.committed_frame)
+        mask = self.frame_buffer[-1] != -1
+        self.committed_frame[mask] = self.frame_buffer[-1][mask]
+        return self.to_cv_frame(self.committed_frame)
 
-k = 10
-frames_average_pooled = []
+    def _update_codebook(self, mask, average_pooled):
+        # Randomly select indices of average_pooled to speed up
+        C, H, W = average_pooled.shape
+        assert H == mask.shape[0]
+        assert W == mask.shape[1]
+        for i in range(0,H,20):
+            for j in range(0,W,20):
+                code = average_pooled[:, i, j]
+                match = self._find_closest_code(code, create=True)
+                if mask[i][j] > 0:
+                    match.obstacle_freq += 1
+                else:
+                    match.board_freq += 1
 
+    def _most_likely_obstacle(self, code):
+        return code.board_freq > 5 and code.obstacle_freq / code.board_freq > 2
 
-def process_frame(frame):
-    print('Deprecated function. Use FrameBuffer class instead.')
-    global frames, frames_average_pooled, frame_counters
+    def _most_likely_board(self, code):
+        return code.obstacle_freq > 5 and code.board_freq / code.obstacle_freq > 2
 
-    frame_t = torch.tensor(frame, dtype=float).permute(2, 0, 1)
-    C, H, W = frame_t.shape
+    def _find_closest_code(self, x, create=False):
 
-    frames.append(frame_t)
+        add_code = True
+        min_dist = float("inf")
+        closest_code = None
+        target = (int(x[0]), int(x[1]), int(x[2]))
+        # print("target", target)
+        # print()
+        for r in range(
+            max(0, target[0] - self.codebook_distance_eps),
+            min(255, target[0] + self.codebook_distance_eps),
+        ):
+            for g in range(
+                max(0, target[1] - self.codebook_distance_eps),
+                min(255, target[1] + self.codebook_distance_eps),
+            ):
+                for b in range(
+                    max(0, target[2] - self.codebook_distance_eps),
+                    min(255, target[2] + self.codebook_distance_eps),
+                ):
+                    if (r, g, b) in self.codebook:
+                        add_code = False
+                        code = self.codebook[(r, g, b)]
+                        dist = torch.abs(code.code - x).sum() / 3
+                        if dist < min_dist:
+                            min_dist = dist
+                            closest_code = code
 
-    average_pool = nn.AvgPool2d((k, k), stride=(k, k))(frame_t)
-    frames_average_pooled.append(average_pool)
+        if create and add_code:
+            self.codebook[target] = Code(x, 0, 0, len(self.codebook))
+            closest_code = self.codebook[target]
+            print("adding code", len(self.codebook))
+        return closest_code
 
-    if len(frames) < num_frames:
-        return frame
+    # def update_code(code, match_idx):
+    #     match = obstacle_codebook_freq[match_idx]
+    #     freq = obstacle_codebook_freq[match_idx]
+    #     obstacle_codebook_freq[match_idx] = (freq*match + code)/ (freq+1)
+    #     obstacle_codebook_freq[match_idx]+=1
 
-    if len(frames) > num_frames:
-        frames = frames[1:]
-        frames_average_pooled = frames_average_pooled[1:]
-
-
-
-    curr_t = (num_frames-1)
-
-    curr_average_pooled_frame = frames_average_pooled[curr_t]
-
-    average_delta = torch.abs(curr_average_pooled_frame - torch.stack(frames_average_pooled)).sum(axis=1).mean(
-        axis=0) / 3  # (N, C, H, W) => (N, H, W) => (H, W)
-    mask = average_delta > 2
-    mask = nn.Upsample((H, W))(mask.unsqueeze(0).unsqueeze(0) + 0.0).squeeze()
-
-    frames[curr_t][:, mask > 0] = 255
-    ret = frames[curr_t]
-    return ret.permute(1, 2, 0).numpy().astype(np.uint8)
+    def _update_mask(self, mask, average_pooled):
+        C, H, W = average_pooled.shape
+        for i in range(H):
+            for j in range(W):
+                code = average_pooled[:, i, j]
+                match = self._find_closest_code(code, create=False)
+                if match != None:
+                    if self._most_likely_obstacle(match):
+                        mask[i, j] = 1
+                    elif self._most_likely_board(match):
+                        mask[i, j] = 0
